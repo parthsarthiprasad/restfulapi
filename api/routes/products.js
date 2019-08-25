0e6dedfbb6ad238e6413e3f992f8a24fca6815e2
[parthsarthiprasad] <[mepsp2000@gmail.com]>
2019-08-25 14:21:26 +0530

    models file for mongoose added and linked to products file

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const Product  = require('../models/product');
 router.get('/' ,(req,res,next)=>{
     res.status(200).json({
         message: 'handling GET requests to /products'    
@@ -9,9 +12,27 @@ router.get('/' ,(req,res,next)=>{
 });
 
 router.post('/' ,(req,res,next) =>{
-    const product ={ name: req.body.name,
+    const product ={ 
+                     name: req.body.name,
                      price: req.body.price
     };
+
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
+        name: req.body.name,
+        price: req.body.price
+    });
+    //product.save().exec();
+    // exec helps to return a promise , else product.save with require a callback
+    //exec is a function of mongoose , not require for save funtion
+    //product.save(err ,result)
+    product
+    .save()
+    .then(result=>{
+        console.log(result;)
+    })
+    .catch(err =>console.log(err));
+    
     res.status (200).json({
         message: 'handling POST requests to /products' ,
         createdProduct: product
