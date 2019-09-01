daa70507121d7b2208a23301ae536b7a43601147
[parthsarthiprasad] <parthp.co18@nsut.ac.in>
2019-09-01 23:24:33 +0530

    order model with reference added

--- api/routes/order.js
+++ api/routes/order.js
@@ -1,46 +1,67 @@
 const express = require('express');
 const router = express.Router();
-router.get('/' ,(req,res,next)=>{
+const mongoose = require('mongoose');
+
+const Order = require('../models/order.js')
+router.get('/', (req, res, next) => {
     res.status(200).json({
-        message: ' order fetched'    
+        message: ' order fetched'
     });
 
 
 });
 
-router.post('/' ,(req,res,next) =>{
-    const order ={
-        productId: req.body.productId ,
-        quantity : req.body.quantity
-    }
-    res.status (201).json({
+router.post('/', (req, res, next) => {
+    const order = new Order({
+        _id: mongoose.Types.ObjectId(),
+        quantity: req.body.quantity,
+        product: req.body.productId
+    });
+    order
+        .save()
+        .exec()
+        .then(result => {
+            console.log(result);
+            res.status(201).json(result);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            })
+
+        })
+    // const order = {
+    //     productId: req.body.productId,
+    //     quantity: req.body.quantity
+    // }
+    res.status(201).json({
         message: ' order created',
         order: order
     })
 });
 
-router.get('/:orderId' , (req,res,next)=>{
+router.get('/:orderId', (req, res, next) => {
     const id = req.params.productId;
-    if(id=== 'special'){
+    if (id === 'special') {
         res.status(200).json({
-           message: 'you discovered the special id' ,
+            message: 'you discovered the special id',
             orderID: req.params.orderID
         })
-    }
-    else {
+    } else {
         res.status(200).json({
             message: "you passed an ID"
         })
     }
 })
 
-router.patch('/:orderid' ,(req,res,next)=>{
+router.patch('/:orderid', (req, res, next) => {
     res.status(200).json({
         message: 'update product|'
     })
 })
 
-router.delete('/:orderid' ,(req,res,next)=>{
+router.delete('/:orderid', (req, res, next) => {
     res.status(200).json({
         message: 'delete order|'
     })
