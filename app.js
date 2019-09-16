a8800351ab364200252f6f6b0922e57c5af92fc9
[parthsarthiprasad] <parthp.co18@nsut.ac.in>
2019-09-17 03:06:57 +0530

    mergeconflict resolved

--- app.js
+++ app.js
@@ -62,13 +62,5 @@ app.use((req,res,next)=>{
     
 })
 
-app.use((error, req,res,next)=>{
-    res.status(error.status||500)
-    res.json({
-        error: {
-            message: error.message
-        }
-    });
-});
 
 module.exports = app;
